bd631ed641b51e79fde987e89ee87c60a98c70ec
mdipirro <matteodipirro@gmail.com>
2017-05-13 16:13:10 +0200

    Config server

---
+++ quizFightServer.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:admin@ds137441.mlab.com:37441/quizfight')
+    .then(() =>  console.log('connection succesful'))
+    .catch((err) => console.error(err));
+
+var app = express();
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var routes = require('./routes/routes');
+app.use('/', routes);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
