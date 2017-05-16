430a2cc0750fa9da1ce61846b1c55b0dc3d8df34
mdipirro <matteodipirro@gmail.com>
2017-05-16 18:56:19 +0200

    Add and factored routes

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var fight = require('./fight');
+var result = require('./result');
+
+module.exports = function(app) {
+	app.route('/fight')
+        .post(fight.post)
+        .put(fight.put);
+
+	app.route('/result')
+        .get(result.get)
+        .put(result.put);
+}
