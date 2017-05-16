64a7d510fb10fd735b0c770efb52d5b6649b8285
mdipirro <matteodipirro@gmail.com>
2017-05-16 18:56:03 +0200

    Add filter by topic

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -5,4 +5,21 @@ var Question = require('../models/questionModel');
 
 var crud = require('./crud')('Question');
 
-module.exports = crud;
+module.exports = {
+    create: crud.create,
+    update: crud.update,
+    delete: crud.delete,
+    getByID: crud.getByID,
+    list: crud.list,
+    getByTopic: function(filter, callback) {
+        Question.getModel().find(filter, function(err, questions) {
+            if (err) {
+                callback(crud.badRequest);
+            } else if (!questions) {
+                callback(crud.notFound);
+            } else {
+                callback(undefined, questions);
+            }
+        });
+    }
+};
