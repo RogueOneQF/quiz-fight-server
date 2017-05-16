147c475810ae12b5b7ab6e3ad24bfd9bb46d7880
EmanueleC <emanuele.carraro.94@gmail.com>
2017-05-16 22:16:39 +0200

    add validation for arrays length

--- models/duelModel.js
+++ models/duelModel.js
@@ -4,15 +4,22 @@ var Duel = (function () {
 
 	var mongoose = require('mongoose');
 
+	var arrayLength = function arrayLength(val) {
+		return val.length == 3;
+	};
+
     var getSchema = function() {
         return new mongoose.Schema({
             user1ID: {type: String, required: true},
             user2ID: {type: String, required: true},
-            quizzes: [{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Quiz',
-                required: true
-            }],
+            quizzes: {
+				type: [{
+		            type: mongoose.Schema.Types.ObjectId,
+		            ref: 'Quiz',
+		            required: true
+		        }],
+				validate: [arrayLength, '{PATH} is not the correct length']
+			},
             user1Score: [{type: Number, min: 0, required: true, default: []}],
             user2Score: [{type: Number, min: 0, required: true, default: []}]
         });
--- models/quizModel.js
+++ models/quizModel.js
@@ -3,14 +3,21 @@
 var Quiz = (function () {
 
 	var mongoose = require('mongoose');
+	
+	var arrayLength = function arrayLength(val) {
+		return val.length == 5;
+	};
 
     var getSchema = function() {
         return new mongoose.Schema({
-            questions: [{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Question',
-                required: true
-            }],
+            questions: {
+				type: [{
+		            type: mongoose.Schema.Types.ObjectId,
+		            ref: 'Question',
+		            required: true
+		        }],
+				validate: [arrayLength, '{PATH} is not the correct length']
+			},
             answers1: [[{type: Number, min: 1, max: 4, required: true, default: []}]],
             answers2: [[{type: Number, min: 1, max: 4, required: true, default: []}]]
         });
