a6530605a98b1442dbaf090fecccff817b5203e7
mdipirro <matteodipirro@gmail.com>
2017-05-16 18:55:48 +0200

    Add method for new rounds

--- controllers/duelController.js
+++ controllers/duelController.js
@@ -5,4 +5,33 @@ var Duel = require('../models/duelModel');
 
 var crud = require('./crud')('Duel');
 
-module.exports = crud;
+module.exports = {
+    create: crud.create,
+    update: crud.update,
+    delete: crud.delete,
+    getByID: crud.getByID,
+    list: crud.list,
+    addRound: function(duelID, quizID, callback) {
+        crud.getByID(duelID, function(err, duel) {
+            if (err) {
+                callback(crud.badRequest);
+			} else if(!duel) {
+                callback(crud.notFound);
+            } else {
+                duel.quizzes.push(quizID);
+                crud.update({
+                    'elementID': duelID,
+                    'element': duel
+                }, function(err, oldDuel) {
+                    if (err) {
+                        callback(crud.badRequest);
+    				} else if (!oldDuel) {
+                        callback(crud.notFound);
+                    } else {
+    					callback(undefined, duel);
+    				}
+                });
+			}
+        });
+    }
+};
