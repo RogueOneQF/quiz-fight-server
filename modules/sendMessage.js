cbe68cb86a4c37422f890fe0ac474cfc9748a1e8
mdipirro <matteodipirro@gmail.com>
2017-05-19 14:45:20 +0200

    Add message sending draft

---
+++ modules/sendMessage.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function(player, winner, score, opponentsName) {
+    var message, title;
+    if (winner) {
+        message = "You defeated " + opponentsName + " " + score.player + " - " + score.opponent;
+        title = "You won :)";
+    } else {
+        message = opponentsName " defeated you " + score.opponent + " - " + score.player;
+        title = "You lost :(";
+    }
+    var payload = {
+        'data': {
+            'tie':              (winner === null),
+            'winner':           winner,
+            'playerScore':      score.player,
+            'opponentScore':    score.opponent
+        },
+        'notification': {
+            'title': title,
+            'body': message
+        }
+    };
+
+    admin.messaging().sendToDevice(player, payload)
+        .then(function(response) {
+            console.log("Successfully sent message:", response);
+        })
+        .catch(function(error) {
+            console.log("Error sending message:", error);
+        });
+};
