091b9c4feb5ae22c2c38b542028b47935194d6fe
mdipirro <matteodipirro@gmail.com>
2017-05-19 14:44:59 +0200

    Add user model

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var User = (function () {
+
+	var mongoose = require('mongoose');
+
+	var getSchema = function() {
+        return new mongoose.Schema({
+            googleUsername: {type: String, required: true},
+            // different devices have different tokens
+            tokens: [{type: String, required: true}]
+        });
+    };
+
+    var model = mongoose.model('User', getSchema());
+
+	return  {
+		getSchema: getSchema,
+		getModel: function() {
+			return model;
+		}
+	};
+
+})();
+
+module.exports = User;
