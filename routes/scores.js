76b91c29fd76176c66db9cf42aa64840281b85af
mdipirro <matteodipirro@gmail.com>
2017-05-28 19:04:43 +0200

    Make duelID array if it is not

--- routes/scores.js
+++ routes/scores.js
@@ -4,7 +4,8 @@ var duels = require('../controllers/duelController');
 var errorHandler = require('../modules/errorHandler');
 
 module.exports = function(req, res) {
-    duels.getScoresByIDs(req.query.duelID, req.query.playerID, function(err, scores) {
+    var ids = (req.query.duelID instanceof Array) ? req.query.duelID : [req.query.duelID];
+    duels.getScoresByIDs(ids, req.query.playerID, function(err, scores) {
         if (err) {
             errorHandler(res, err);
         } else {
